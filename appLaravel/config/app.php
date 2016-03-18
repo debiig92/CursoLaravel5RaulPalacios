93255bf0ae21ee79ea9628dcc23014c4c4ca10c0
felixacdc <pfmata01@gmail.com>
2016-03-18 00:12:46 -0600

    [add] 04 - Curso de Laravel 5, Configurando el Proyecto

--- appLaravel/config/app.php
+++ appLaravel/config/app.php
@@ -39,7 +39,7 @@ return [
 	|
 	*/
 
-	'timezone' => 'UTC',
+	'timezone' => 'America/Guatemala',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
 	|
 	*/
 
-	'locale' => 'en',
+	'locale' => 'es',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -139,11 +139,11 @@ return [
 		/*
 		 * Application Service Providers...
 		 */
-		'App\Providers\AppServiceProvider',
-		'App\Providers\BusServiceProvider',
-		'App\Providers\ConfigServiceProvider',
-		'App\Providers\EventServiceProvider',
-		'App\Providers\RouteServiceProvider',
+		'Cinema\Providers\AppServiceProvider',
+		'Cinema\Providers\BusServiceProvider',
+		'Cinema\Providers\ConfigServiceProvider',
+		'Cinema\Providers\EventServiceProvider',
+		'Cinema\Providers\RouteServiceProvider',
 
 	],
 
