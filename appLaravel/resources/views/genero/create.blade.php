1f7bd4c627c14102837bafc7a189bd38b0daa82b
felixacdc <pfmata01@gmail.com>
2016-03-23 22:01:13 -0600

    [add] 22 - Curso de Laravel 5.1, Crear con AJAX

---
+++ appLaravel/resources/views/genero/create.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.admin')
+
+@section('content')
+
+    <h2>Creacion de Generos</h2>
+
+    {!!Form::open()!!}
+
+        <div id="msj-success" class="alert alert-success alert-dismissible" role="alert" style="display:none">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <strong> Registro Agragado Correctamente</strong>
+        </div>
+
+        <input type="hidden" name="_token" value="{{ csrf_token() }}" id="token">
+
+        @include('genero.form.genero')
+
+        {!!link_to('#', $title = 'Registrar', $attributes = ['id' => 'registro', 'class' => 'btn btn-primary'], $secure = null)!!}
+
+    {!!Form::close()!!}
+
+@stop
---
+++ appLaravel/resources/views/genero/form/genero.blade.php
@@ -0,0 +1,4 @@
+<div class="form-group">
+    {!!Form::label('genre', 'Nombre')!!}
+    {!!Form::text('genre', null, array('id' => 'genre', 'class' => 'form-control', 'placeholder' => 'Ingresa el genero'))!!}
+</div>
